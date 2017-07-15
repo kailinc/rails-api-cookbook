1ab7ab19a216ec4b6d9852ce90bf211b8fd13326
kailinc <kailinc@bu.edu>
2017-07-15 11:11:48 -0400

    Practiced Ruby Rails: Updating Book -created curl script to update book -created destroy method in books controller

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,11 +1,21 @@
 class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:show, :destroy]
+
   def index
     @ingredients = Ingredient.all
     render json: @ingredients
   end
 
   def show
-    @ingredient = Ingredient.find(params[:id])
     render json: @ingredient
+  end
+
+  def destroy
+    @ingredient.destroy
+    head :no_content
+  end
+
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
   end
 end
